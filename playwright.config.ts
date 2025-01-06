d8eacbf5db0ee8265bd1a9e814d22a0bcde2fd8e
LDenysiuk <liudmyla@studysmarter.de>
2025-01-06 02:10:40 +0200

    c

--- playwright.config.ts
+++ playwright.config.ts
@@ -28,7 +28,7 @@ export default defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   //reporter: 'html',
   //reporter: [['html'], ['line']],
-  reporter:[['allure-playwright', {outputFolder: './test-results'}], ['line']],
+  reporter:[['allure-playwright', {outputFolder: 'test-results'}], ['line']],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
 
  // globalSetup: require.resolve('./utils/global-setup'),
